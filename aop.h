e8622b8f3b6584f064affe61ac6c5ca52d1a06d2
gerald croes <gerald@croes.org>
2012-06-07 17:02:58 +0200

    Added license and credits in the source files

--- aop.h
+++ aop.h
@@ -1,3 +1,21 @@
+/*
+/+----------------------------------------------------------------------+
+ | AOP                                                                  |
+ +----------------------------------------------------------------------+
+ | Copyright (c) 2012 Julien Salleyron, Gérald Croës                    |
+ +----------------------------------------------------------------------+
+ | This source file is subject to version 3.01 of the PHP license,      |
+ | that is bundled with this package in the file LICENSE, and is        |
+ | available through the world-wide-web at the following url:           |
+ | http://www.php.net/license/3_01.txt.                                 |
+ | If you did not receive a copy of the PHP license and are unable to   |
+ | obtain it through the world-wide-web, please send a note to          |
+ | license@php.net so we can mail you a copy immediately.               |
+ +----------------------------------------------------------------------+
+ | Author: Julien Salleyron <julien.salleyron@gmail.com>                |
+ +----------------------------------------------------------------------+
+*/
+
 #ifndef PHP_AOP_H
 #define PHP_AOP_H 1
 
